24294d595c92515640123a2299d37bebc48709d6
RnDevelover <me@ahmetyildirim.com.tr>
2021-03-19 13:28:09 +0300

    Remove dockbarx related coding. Add new panel configuration.

--- mod.sh
+++ mod.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-sudo add-apt-repository ppa:xuzhen666/dockbarx
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install --no-install-recommends xfce4-dockbarx-plugin
+#sudo apt-get update
+sudo add-apt-repository -y ppa:linuxuprising/shutter
 sudo apt-get install xmonad
-sudo apt-get install gnome-terminal zeitgeist python-xdg vala-panel-appmenu appmenu-gtk3-module appmenu-gtk2-module appmenu-qt gnome-terminal xmonad shutter xfce4-appmenu-plugin xfce4-systemload-plugin arc-theme vim xfpanel-switch google-chrome-stable atril
+sudo apt-get install xfpanel-switch
+sudo apt-get install gnome-terminal zeitgeist python-xdg vala-panel-appmenu appmenu-gtk3-module appmenu-gtk2-module gnome-terminal xmonad shutter xfce4-appmenu-plugin xfce4-systemload-plugin arc-theme vim xfpanel-switch google-chrome-stable atril
 sudo cp xfceSettings/ubuntu-icon.png /usr/share/icons/ubuntu-icon.png
 mkdir ~/.xmonad
 mkdir ~/.config
@@ -27,11 +25,6 @@ xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
 xfconf-query -c xsettings -p /Gtk/Modules -n -t string -s "appmenu-gtk-module"
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
-gconftool --load dockbarx/dockbarx.xml
-pw=$(pwd)
-chdir ~ 
-tar -xzvf "$pw/dockbarx/dockbarx.tar.gz"
-cd "$pw"
 sudo cp launchers/gnome-terminal.desktop /usr/share/applications
-python3  /usr/share/xfpanel-switch/xfpanel-switch/xfpanel-switch.py load xfceSettings/panels.tar.bz2
+python3 /usr/share/xfce4-panel-profiles/xfce4-panel-profiles/xfce4-panel-profiles.py load xfceSettings/panels.tar.bz2
 cat launchers/gnometerminalsettings.txt | dconf load /org/gnome/terminal/legacy/profiles:/
