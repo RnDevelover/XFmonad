7180503fb1a7650c3d1af3fefeb119d099ccd67a
RnDevelover <me@ahmetyildirim.com.tr>
2022-05-06 23:40:17 +0200

    Minor

--- newMod.sh
+++ newMod.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd ~
 sudo apt-get install xmonad suckless-tools build-essential rxvt-unicode lightdm net-tools network-manager network manager-gnome network-manager-openconnect arc-theme lxappearance git
 mkdir Downloads
 cd Downloads
@@ -7,4 +8,4 @@ sudo apt-get install ./google-chrome-stable_current_amd64.deb
 cd ..
 mkdir ~/.xmonad
 cd ~/.xmonad
-
+wget https://pastebin.com/raw/zbLbxqst -O xmonad.hs
