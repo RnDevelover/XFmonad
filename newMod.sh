dc3ae90c3109e5b9c6fc0531e8edb08f00223715
RnDevelover <me@ahmetyildirim.com.tr>
2022-05-06 23:31:01 +0200

    starting new Mod

---
+++ newMod.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get install xmonad suckless-tools build-essential rxvt-unicode lightdm net-tools network-manager network manager-gnome network-manager-openconnect arc-theme lxappearance git
+mkdir Downloads
+cd Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
+cd ..
+mkdir ~/.xmonad
+cd ~/.xmonad
+
