0ec3b59520b661ee54bca17f4b395cf9e87b1692
RnDevelover <me@ahmetyildirim.com.tr>
2022-05-09 00:09:32 +0200

    Add to apt

--- newMod.sh
+++ newMod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-sudo apt-get install xmonad suckless-tools build-essential rxvt-unicode lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git
+sudo apt-get install xmonad suckless-tools build-essential rxvt-unicode lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git htop pcmanfm pulseaudio pulseaudio-utils xmobar trayer xfce4-power-manager feh xscreensaver
 mkdir Downloads
 cd Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
