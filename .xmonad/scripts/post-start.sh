8a0585bb6708ef408d70abe07fe7b973dd66106d
toxicity <toxicity@toxicity.toxicity>
2022-05-15 01:09:56 +0200

    Add notification starter to scripts

--- .xmonad/scripts/post-start.sh
+++ .xmonad/scripts/post-start.sh
@@ -1,3 +1,4 @@
 sleep 10 
 touch ~/.xmonad/picom.conf
-/usr/bin/volumeicon || /usr/bin/volumeicon </dev/null >/dev/null 2>/dev/null 
+/usr/lib/notification-daemon/notification-daemon </dev/null >/dev/null 2>&1 &
+/usr/bin/volumeicon || /usr/bin/volumeicon </dev/null >/dev/null 2>/dev/null
