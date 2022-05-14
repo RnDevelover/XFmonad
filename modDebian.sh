b2776f2470f7c72bf6f19886a652677ed81d9017
RnDevelover <me@ahmetyildirim.com.tr>
2022-05-14 08:54:17 +0200

    Try to fix network problem

--- modDebian.sh
+++ modDebian.sh
@@ -3,13 +3,15 @@ CWD=$(pwd)
 cd ~
 sudo sed -r -i 's/^deb(.*)$/deb\1 contrib/g' /etc/apt/sources.list
 sudo sed -r -i 's/^deb(.*)$/deb\1 non-free/g' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install vim suckless-tools build-essential lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git htop pcmanfm volumeicon-alsa xmobar trayer xfce4-power-manager feh fonts-font-awesome ttf-ubuntu-font-family fonts-mononoki arandr ssh system-config-printer cups xbacklight xfce4-power-manager xautolock xtrlock curl cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 pluma xterm picom nitrogen network-manager-config-connectivity-debian network-manager-vpnc network-manager-vpnc-gnome network-manager-openconnect-gnome unzip libx11-dev libxrandr-dev libxft-dev xdotool
 mkdir Downloads
 cd Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get install vim suckless-tools build-essential lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git htop pcmanfm volumeicon-alsa xmobar trayer xfce4-power-manager feh fonts-font-awesome ttf-ubuntu-font-family fonts-mononoki arandr ssh system-config-printer cups xbacklight xfce4-power-manager xautolock xtrlock curl cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 pluma xterm picom nitrogen network-manager-config-connectivity-debian network-manager-vpnc network-manager-vpnc-gnome network-manager-openconnect-gnome unzip libx11-dev libxrandr-dev libxft-dev xdotool
 sudo apt-get install ./google-chrome-stable_current_amd64.deb
+mkdir ~/.local
 mkdir ~/.local/bin
 cd $CWD
+sleep 20
 ./modContinue.sh
 sudo ./setupNetwork.sh
