cef90701093b78d7422cb76f015ae828c870af50
toxicity <toxicity@toxicity.toxicity>
2022-05-15 00:41:52 +0200

    Latest updates after Elitebook840 installation

--- modDebian.sh
+++ modDebian.sh
@@ -7,7 +7,7 @@ mkdir Downloads
 cd Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get update
-sudo apt-get install vim suckless-tools build-essential lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git htop pcmanfm volumeicon-alsa xmobar trayer xfce4-power-manager feh fonts-font-awesome ttf-ubuntu-font-family fonts-mononoki arandr ssh system-config-printer cups xbacklight xfce4-power-manager xautolock xtrlock curl cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 pluma xterm picom nitrogen network-manager-config-connectivity-debian network-manager-vpnc network-manager-vpnc-gnome network-manager-openconnect-gnome unzip libx11-dev libxrandr-dev libxft-dev xdotool wireless-tools wpasupplicant
+sudo apt-get install vim suckless-tools build-essential lightdm net-tools network-manager network-manager-gnome network-manager-openconnect arc-theme lxappearance git htop pcmanfm volumeicon-alsa xmobar trayer xfce4-power-manager feh fonts-font-awesome ttf-ubuntu-font-family fonts-mononoki arandr ssh system-config-printer cups xbacklight xfce4-power-manager xautolock xtrlock curl cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 pluma xterm picom nitrogen network-manager-config-connectivity-debian network-manager-vpnc network-manager-vpnc-gnome network-manager-openconnect-gnome unzip libx11-dev libxrandr-dev libxft-dev xdotool wireless-tools wpasupplicant alsa-tools firmware-intel-sound firmware-iwlwifi
 sleep 20
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
@@ -27,3 +27,5 @@ mkdir ~/.local/bin
 cd $CWD
 ./modContinue.sh
 sudo ./setupNetwork.sh
+echo "defaults.pcm.card 1">/etc/asound.conf
+echo "defaults.ctl.card 1">>/etc/asound.con
--- setupNetwork.sh
+++ setupNetwork.sh
@@ -5,12 +5,6 @@ echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.tun0.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.disable_ipv6 = 1">> /etc/sysctl.d/70-disable-ipv6.conf
-echo "GRUB_DEFAULT=0" > /etc/default/grub
-echo "GRUB_TIMEOUT=5" >> /etc/default/grub
-echo 'GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`' >> /etc/default/grub
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet ipv6.disable=1"' >> /etc/default/grub
-echo 'GRUB_CMDLINE_LINUX="ipv6.disable=1"' >> /etc/default/grub
 sudo sysctl -p -f /etc/sysctl.d/70-disable-ipv6.conf
 service networking restart
 service NetworkManager restart 
-update-grub
