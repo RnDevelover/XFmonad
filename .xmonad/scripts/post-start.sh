53303ed2adff0d3caee8caf9dc8a934ba7cac0dd
toxicity <toxicity@toxicity.toxicity>
2022-05-17 20:44:14 +0200

    Add dunst as notification handler

--- .xmonad/scripts/post-start.sh
+++ .xmonad/scripts/post-start.sh
@@ -1,4 +1,4 @@
 sleep 10 
 touch ~/.xmonad/picom.conf
-/usr/lib/notification-daemon/notification-daemon </dev/null >/dev/null 2>&1 &
+/usr/bin/dunst -config ~/.xmonad/dunstrc/dunstrc </dev/null >/dev/null 2>&1 &
 /usr/bin/volumeicon || /usr/bin/volumeicon </dev/null >/dev/null 2>/dev/null
